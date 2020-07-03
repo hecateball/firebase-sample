694087bc3cd2ed8a302813ff1149371dcd2689ba
yuuki.fukuda <yuuki.fukuda@dena.com>
2020-07-03 16:50:25 +0900

    initial commit

---
+++ shim-vue.d.ts
@@ -0,0 +1,4 @@
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue'
+  export default ComponentOptions
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    }
+  },
+})
+</script>
