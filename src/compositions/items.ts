3f1bdd60c2d02c7e52f48fe99524b92eedb783c8
hecateball <hecateball@gmail.com>
2020-07-16 23:20:00 +0900

    image upload

--- src/compositions/items.ts
+++ src/compositions/items.ts
@@ -1,23 +1,36 @@
-import { ref, readonly, onBeforeUnmount } from 'vue'
+import { ref, readonly, onBeforeUnmount, reactive } from 'vue'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 export type Item = {
   id?: string
   name: string
   description: string
   price: number
+  image: {
+    url: string
+  }
   createdAt: Date
 }
 
-export type ItemInput = Omit<Item, 'createdAt'>
+export type ItemInput = {
+  name: string
+  description: string
+  price: number
+  image: {
+    extension: string
+    dataURL: string
+  }
+}
 
 const converter: firebase.firestore.FirestoreDataConverter<Item> = {
   toFirestore: (item: Item): firebase.firestore.DocumentData => ({
     name: item.name,
     description: item.description,
     price: item.price,
+    image: item.image,
     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
   }),
   fromFirestore: (
@@ -28,6 +41,7 @@ const converter: firebase.firestore.FirestoreDataConverter<Item> = {
     name: snapshot.get('name'),
     description: snapshot.get('description'),
     price: snapshot.get('price'),
+    image: snapshot.get('image'),
     createdAt: snapshot.get('createdAt', options).toDate(),
   }),
 }
@@ -65,16 +79,26 @@ export const useItems = (uid?: string) => {
 
 export const createItem = async (item: ItemInput) => {
   try {
-    await firebase
+    const reference = firebase
       .firestore()
       .collection('users')
       .doc(firebase.auth().currentUser.uid)
       .collection('items')
       .withConverter(converter)
-      .add({
-        ...item,
-        createdAt: new Date(),
-      })
+      .doc()
+    //画像のアップロード
+    const image = firebase
+      .storage()
+      .ref(`${firebase.auth().currentUser.uid}/images`)
+      .child(`${reference.id}.${item.image.extension}`)
+    const url = await image
+      .putString(item.image.dataURL, 'data_url')
+      .then(async (): Promise<string> => await image.getDownloadURL())
+    await reference.set({
+      ...item,
+      image: { url },
+      createdAt: new Date(),
+    })
   } catch (error) {
     console.error(error)
   }
